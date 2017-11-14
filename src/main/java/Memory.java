d2c7ba28b045ddab9527a691bf774f1b1b8d3122
Dmytry Dyachenko <dmytry.dyachenko@teamdev.local>
2017-11-14 17:12:15 +0200

    add unworking test, for understanding the correct way of thinking with execute code

--- src/main/java/Memory.java
+++ src/main/java/Memory.java
@@ -7,6 +7,15 @@ public class Memory {
     private ArrayDeque<Integer> loopStart = new ArrayDeque<>();
     private int index = 0;
     private int[] mainMemoryArray = new int[50];
+
+    public int[] getMainMemoryArray() {
+        return mainMemoryArray;
+    }
+
+    public void setMainMemoryArray(int[] mainMemoryArray) {
+        this.mainMemoryArray = mainMemoryArray;
+    }
+
     /***Array, which will be used like main memory container */
     private String outputString = new String();
 
@@ -16,6 +25,14 @@ public class Memory {
 
     public void setOutputString(String outputString) {
         this.outputString = outputString;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
 
     public void implementCommands(BrainFuckCommand brainFuckCommand) {
@@ -28,7 +45,7 @@ public class Memory {
     public void decrement() {
     }
 
-    public void moveCarretLeft() {
+    public void moveCaretLeft() {
 
     }
 
---
+++ src/test/java/MainCommandsTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * The class for testing main commands of brainFuck language
+ */
+public class MainCommandsTest {
+    Memory memoryInstance = new Memory();
+
+    @Test
+    public void moveCaretRightTest(){
+        memoryInstance.setIndex(10);
+        memoryInstance.moveCarretRight();
+        assertEquals(11,memoryInstance.getIndex());
+    }
+
+    @Test
+    public void moveCaretLeftTest(){
+        memoryInstance.setIndex(11);
+        memoryInstance.moveCaretLeft();
+        assertEquals(10,memoryInstance.getIndex());
+    }
+
+    @Test
+    public void incrementTest(){
+        int[] array = new int[50];
+        memoryInstance.setIndex(10);
+        memoryInstance.setMainMemoryArray(array);
+        memoryInstance.increment();
+        assertEquals(1,memoryInstance.getMainMemoryArray()[memoryInstance.getIndex()]);
+    }
+    @Test
+    public void decrementTest(){
+        int[] array = new int[50];
+        memoryInstance.setIndex(10);
+        memoryInstance.setMainMemoryArray(array);
+        memoryInstance.decrement();
+        assertEquals(0,memoryInstance.getMainMemoryArray()[memoryInstance.getIndex()]+1);
+    }
+}
