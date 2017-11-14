d9ba87dc5cce8e9a6af47865e08f1c96418bd854
Dmytry Dyachenko <dmytry.dyachenko@teamdev.local>
2017-11-14 16:50:29 +0200

    memory manipulation full description{

--- src/main/java/Memory.java
+++ src/main/java/Memory.java
@@ -4,5 +4,47 @@ import java.util.ArrayDeque;
  * Every manipulation with command
  */
 public class Memory {
-    private ArrayDeque<Integer> loopStart  = new ArrayDeque<>();
+    private ArrayDeque<Integer> loopStart = new ArrayDeque<>();
+    private int index = 0;
+    private int[] mainMemoryArray = new int[50];
+    /***Array, which will be used like main memory container */
+    private String outputString = new String();
+
+    public String getOutputString() {
+        return outputString;
+    }
+
+    public void setOutputString(String outputString) {
+        this.outputString = outputString;
+    }
+
+    public void implementCommands(BrainFuckCommand brainFuckCommand) {
+
+    }
+
+    public void increment() {
+    }
+
+    public void decrement() {
+    }
+
+    public void moveCarretLeft() {
+
+    }
+
+    public void moveCarretRight() {
+
+    }
+
+    public void startTheLoop() {
+
+    }
+
+    public void endTheLoop() {
+
+    }
+
+    public void output() {
+
+    }
 }
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -5,7 +5,7 @@ public class Parser {
     public BrainFuckCommand[] parseToCommand(String brainFuckString){
         char[] brainFuckCharackters = brainFuckString.toCharArray();
         BrainFuckCommand[] brainFuckCommands = new BrainFuckCommand[brainFuckCharackters.length];
-        
+
         return brainFuckCommands;
     }
 }
